8aa0ab34e46d3013aa02e3bf6fd52ef3ee9374e8
Kimel-PK <kimel@op.pl>
2022-06-17 14:50:21 +0200

    Zestaw 10 - Poprawienie wyglądu kodu

--- Zestaw 10/ZadanieA.cpp
+++ Zestaw 10/ZadanieA.cpp
@@ -8,38 +8,28 @@
 using namespace std;
 
 map<string, map<string, int>> FloydWarshall (ADTgraph& graf) {
+	// https://pl.wikipedia.org/wiki/Algorytm_Floyda-Warshalla
 	
 	map<string, map<string, int>> d;
 	
-	// dla każdego wierzchołka v1 w V[G] wykonaj
 	for (string v1 : graf.getVertices()) {
-		// dla każdego wierzchołka v2 w V[G] wykonaj
 		for (string v2 : graf.getVertices()) {
-			// d[v1][v2] = nieskończone
-			d [v1][v2] = INT_MAX;
+			d[v1][v2] = INT_MAX;
 		}
-		// d[v1][v1] = 0
-		d [v1][v1] = 0;
+		d[v1][v1] = 0;
 	}
-	// dla każdej krawędzi (v1,v2) w E[G]
 	for (string v1 : graf.getVertices()) {
 		for (string v2 : graf.getVertices()) {
 			if (graf.getEdgeValue (v1, v2) > -1) {
-				// d[v1][v2] = w(v1,v2)
-				d [v1][v2] = graf.getEdgeValue (v1, v2);
+				d[v1][v2] = graf.getEdgeValue (v1, v2);
 			}
 		}
 	}
-	// dla każdego wierzchołka u w V[G] wykonaj
 	for (string u : graf.getVertices()) {
-		// dla każdego wierzchołka v1 w V[G] wykonaj
 		for (string v1 : graf.getVertices()) {
-			// dla każdego wierzchołka v2 w V[G] wykonaj
 			for (string v2 : graf.getVertices()) {
-				// jeżeli d[v1][v2] > d[v1][u] + d[u][v2] to
-				if (d [v1][v2] > d [v1][u] + d [u][v2] && d [v1][u] != INT_MAX && d [u][v2] != INT_MAX) {
-					// d[v1][v2] = d[v1][u] + d[u][v2]
-					d [v1][v2] = d [v1][u] + d [u][v2];
+				if (d[v1][v2] > d[v1][u] + d[u][v2] && d[v1][u] != INT_MAX && d[u][v2] != INT_MAX) {
+					d[v1][v2] = d[v1][u] + d[u][v2];
 				}
 			}
 		}
